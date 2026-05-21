d98de29b1a34240887dfe748af928062935b34da
salutzd <zdonghh@outlook.com>
2026-05-21 14:35:34 +0800

    update main.c in office

--- src/main.c
+++ src/main.c
@@ -13,7 +13,7 @@ int main(int argc, char ** argv) {
     lv_init();
     sdl_hal_init(320, 240);
 
-    start_myui();  // test UI
+    start_myui();  // test UIs
 
     while (1) {
         uint32_t sleep_time_ms = lv_timer_handler();
